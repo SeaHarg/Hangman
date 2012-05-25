a4a01233742c78a3e144dce815312a2ca3c04de0
Sean Hargrave <sean.hargrave@hotmail.com>
2012-05-24 22:15:51 -0400

    Basic UI
    Created lables added click event

--- prjHangman/prjHangman/Form1.Designer.cs
+++ prjHangman/prjHangman/Form1.Designer.cs
@@ -28,22 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mtbAnswer = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // mtbAnswer
+            // 
+            this.mtbAnswer.Enabled = false;
+            this.mtbAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbAnswer.Location = new System.Drawing.Point(68, 28);
+            this.mtbAnswer.Name = "mtbAnswer";
+            this.mtbAnswer.Size = new System.Drawing.Size(286, 44);
+            this.mtbAnswer.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(537, 420);
+            this.Controls.Add(this.mtbAnswer);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox mtbAnswer;
     }
 }
 
--- prjHangman/prjHangman/Form1.cs
+++ prjHangman/prjHangman/Form1.cs
@@ -12,19 +12,69 @@ namespace prjHangman
     public partial class Form1 : Form
     {
         private dictionary Words = new dictionary();
+        private string GuessedLetters = "";
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void  InitGame()
         {
             Words.GetWord();
+            for (int c = 65; c < 91; c++)
+            {
+                Label Letter = new Label();
+                Letter.Text = ((char)c).ToString();
+                Letter.Top = c < 80 ? 300 : 340;
+                Letter.Left = c < 80 ? (c - 65) * 34 + 12 : (c - 80) * 44 + 23;
+                Letter.Font = mtbAnswer.Font;
+                Letter.AutoSize = true;
+                Letter.Click += new EventHandler(Letter_Click);
+                this.Controls.Add(Letter);
+
+            }
+        }
+
+        void Letter_Click(object sender, EventArgs e)
+        {
+            Label ClickedLetter = sender as Label;
+            MessageBox.Show(ClickedLetter.Text);
+            ClickedLetter.Visible = false;
+            GuessedLetters += ClickedLetter.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitGame();
         }
 
         private void Form1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (GuessedLetters.Contains(e.KeyChar.ToString().ToUpper()))
+            {
+                MessageBox.Show("Already guessed");
+            }
+            else
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c.GetType() == typeof(Label))
+                    {
+                        if (c.Text.ToUpper() == e.KeyChar.ToString().ToUpper())
+                        {
+                            Letter_Click(c, EventArgs.Empty);
+                        }
+                    }
+                }
+            }
 
         }
 
